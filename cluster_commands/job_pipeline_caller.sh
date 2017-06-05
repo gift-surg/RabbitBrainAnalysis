85569eb3c036073db1b00ea9065cd2a14d6d4d84
Sebastiano Ferraris <sebastiano.ferraris@gmail.com>
2017-06-05 17:20:40 +0100

    cluster commands update

---
+++ cluster_commands/job_pipeline_caller.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# path to virtual env, to run python:
+/home/ferraris/py_venvs/v2/bin/python ./call_executer.py $SUBJECT
+
--- cluster_commands/job_pipeline_project.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# path to virtual env, to run python:
-/Applications/python_virtualenvs/clean2_7/bin/python ./call_executer.py $SUBJECT
-
