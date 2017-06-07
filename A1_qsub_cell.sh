05ddbae7958c2d96add5bd4f00791249ca013762
Sebastiano Ferraris <sebastiano.ferraris@gmail.com>
2017-06-07 11:06:27 +0100

    small

--- A1_qsub_cell.sh
+++ A1_qsub_cell.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # echo "submitting the job ${1}"
-qsub -l h_rt=12:00:00 -l tmem=15G -l h_vmem=15G -N "job_sj_${1}" -cwd -v SUBJECT=${1} A2_run_python.sh
+qsub -l h_rt=12:00:00 -l tmem=15G -l h_vmem=15G -N "Job${1}" -cwd -v SUBJECT=${1} A2_run_python.sh
