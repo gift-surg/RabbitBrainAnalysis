cd75fd643ddc5a0a16a7745bb001e4dcf3a53a81
Sebastiano Ferraris <sebastiano.ferraris@gmail.com>
2017-06-07 11:38:17 +0100

    aaa

--- cluster_commands/dummy_call_executer.py
+++ cluster_commands/dummy_call_executer.py
@@ -7,9 +7,9 @@ sys.path.append(dir_path)
 sys.path.append(os.path.dirname(dir_path))
 
 print dir_path
-
+print os.path.dirname(dir_path)
 from pipeline_project.A0_main.main_executer import main_runner
-from definitions import root_study_rabbits
+# from definitions import root_study_rabbits
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', dest='str_input', type=str, required=True)
@@ -18,7 +18,7 @@ args = parser.parse_args()
 
 f = open('../output_{}.txt'.format(args.str_input), 'w+')
 f.writelines(args.str_input + '\n')
-f.writelines(root_study_rabbits + '\n')
+# f.writelines(root_study_rabbits + '\n')
 f.writelines(main_runner.__file__ + '\n')
 f.close()
 
