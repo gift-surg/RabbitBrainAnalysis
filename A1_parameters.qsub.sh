98f655ad77cb24ae5f5fdd914e4460fffc0f4705
Sebastiano Ferraris <sebastiano.ferraris@gmail.com>
2017-06-11 14:35:11 +0100

    update parame

--- A1_parameters.qsub.sh
+++ A1_parameters.qsub.sh
@@ -2,15 +2,17 @@ date
 hostname
 
 #$ -l h_rt=12:00:00
-#$ -l tmem=10G
-#$ -l h_vmem=10G
+#$ -l tmem=20G
+#$ -l h_vmem=30G
 #$ -N "ImgBrainAnalysis"
 #$ -S /bin/bash
 #$ -cwd
-#$ -t 1-4
+#$ -t 1-12
 #$ -e ../z_output/
 #$ -o ../z_output/
 
 SUBJECT=`sed -n ${SGE_TASK_ID}p subjects.txt`
+
+echo $SUBJECT
 
 ./A2_run_python.sh $SUBJECT
