dd2e8a1389c702952c1e0f9d346fd1cd871fbd28
Sebastiano Ferraris <sebastiano.ferraris@gmail.com>
2017-10-10 10:25:01 +0100

    WIP starting modification - avoiding simple resampling to create the S0 roi mask. They are always in different spaces

--- NiftyNet_interface/data_preparation.py
+++ NiftyNet_interface/data_preparation.py
@@ -1,5 +1,6 @@
 import os
 from os.path import join as jph
+import numpy as np
 from collections import OrderedDict
 
 from main_pipeline.A0_main.subject_parameters_manager import get_list_names_subjects_in_template
@@ -15,41 +16,59 @@ The data analysis cannot be performed in the bicommissural for the moment.
 """
 
 
-def prepare_data_subject_list(sj_list, controller):
+def prepare_data_subject_list(sj_list, sj_left_out_list, controller):
 
     check_libraries()
     assert os.path.exists(root_study_rabbits)
 
     pfo_nifty_net_data = jph(root_study_rabbits, 'D_nifty_net')
+    pfo_nifty_net_data_template = jph(pfo_nifty_net_data, 'template')
+    pfo_nifty_net_data_target = jph(pfo_nifty_net_data, 'target')
 
-    if controller['Update_data_folder']:
+    if controller['Update_data_folders']:
 
         if os.path.exists(pfo_nifty_net_data):
-            os.system('rm -r {}'.format(pfo_nifty_net_data))
-        os.system('mkdir {}'.format(pfo_nifty_net_data))
+            os.system('rm -r {}'.format(pfo_nifty_net_data_template))
+            os.system('rm -r {}'.format(pfo_nifty_net_data_target))
+        os.system('mkdir {}'.format(pfo_nifty_net_data_template))
+        os.system('mkdir {}'.format(pfo_nifty_net_data_target))
 
+        # create template
         if controller['Modality'] == 'Mono':
-            for sj in sj_list:
 
+
+
+            # create template folder
+            for sj in sj_list:
                 pfo_sj_original = jph(root_internal_template, sj)
 
                 # cp T1
                 pfi_original_T1 = jph(pfo_sj_original, 'mod', sj + '_T1.nii.gz')
                 assert os.path.exists(pfi_original_T1), pfi_original_T1
-                pfi_new = jph(pfo_nifty_net_data, 'T1_{}.nii.gz'.format(sj))
+                pfi_new = jph(pfo_nifty_net_data_template, 'T1_{}.nii.gz'.format(sj))
                 os.system('cp {0} {1}'.format(pfi_original_T1, pfi_new))
 
                 # cp atlas
                 pfi_original_segm = jph(pfo_sj_original, 'segm',  sj + '_approved.nii.gz')
                 assert os.path.exists(pfi_original_segm), pfi_original_segm
-                pfi_new = jph(pfo_nifty_net_data, 'Atlas_{}.nii.gz'.format(sj))
+                pfi_new = jph(pfo_nifty_net_data_template, 'Atlas_{}.nii.gz'.format(sj))
                 os.system('cp {0} {1}'.format(pfi_original_segm, pfi_new))
 
                 # cp mask
                 pfi_original_segm = jph(pfo_sj_original, 'masks', sj + '_roi_mask.nii.gz')
                 assert os.path.exists(pfi_original_segm), pfi_original_segm
-                pfi_new = jph(pfo_nifty_net_data, 'Mask_{}.nii.gz'.format(sj))
+                pfi_new = jph(pfo_nifty_net_data_template, 'Mask_{}.nii.gz'.format(sj))
                 os.system('cp {0} {1}'.format(pfi_original_segm, pfi_new))
+
+            # create template folder
+            for sj in sj_left_out_list:
+                pfo_sj_original = jph(root_internal_template, sj)
+
+                # cp T1
+                pfi_original_T1 = jph(pfo_sj_original, 'mod', sj + '_T1.nii.gz')
+                assert os.path.exists(pfi_original_T1), pfi_original_T1
+                pfi_new = jph(pfo_nifty_net_data_target, 'T1_{}.nii.gz'.format(sj))
+                os.system('cp {0} {1}'.format(pfi_original_T1, pfi_new))
 
         elif controller['Modality'] == 'Multi':
             print('See TODO list')
@@ -57,20 +76,18 @@ def prepare_data_subject_list(sj_list, controller):
         else:
             raise IOError
 
-    if controller['Copy_data_folder_to_cluster']:
-        pfo_in_cluster_data = '/cluster/project0/fetalsurgery/Data/MRI/KUL_preterm_rabbit_model/data/D_nifty_net'
-        cmd = "rsync -rov --exclude '.DS_Store' {0} ferraris@comic100.cs.ucl.ac.uk:{1}".format(pfo_nifty_net_data,
-                                                                                               pfo_in_cluster_data)
-        os.system(cmd)
-
 
 if __name__ == '__main__':
     print('process T1, local run. ')
 
     controller_ = OrderedDict({'Modality'                    : 'Mono',
-                               'Update_data_folder'          : True,
-                               'Copy_data_folder_to_cluster' : False})
+                               'Update_data_folders'          : False})
 
     list_subjects_in_template = get_list_names_subjects_in_template(pfo_subjects_parameters)
     print list_subjects_in_template
-    prepare_data_subject_list(list_subjects_in_template, controller_)
+
+    # cross validation
+    left_out = ['1702']
+    list_subjects_in_template = list(np.sort(set(list_subjects_in_template)  - set(left_out)))
+
+    prepare_data_subject_list(list_subjects_in_template, left_out, controller_)
